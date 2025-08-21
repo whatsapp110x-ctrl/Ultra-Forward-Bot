c570d72f130ed16781a541dfc5b0e648eed353e4
whatsapp110x-ctrl <whatsapp110x@gmail.com>
2025-08-21 20:43:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ from os import environ
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = environ.get("API_ID", "29756601")
+    API_HASH = environ.get("API_HASH", "967c4656b8dd6a475b7786e3287ba2d3")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8372107408:AAEdnBUYJLmDhOKycjQHsvN30bmKjfCejcY") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://whatsapp110x:whatsapp110x@cluster0.dh2ygtt.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "NastyBot")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7052170756').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002319385484'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/XDemonHunter_Channel") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
